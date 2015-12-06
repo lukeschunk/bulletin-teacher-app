c8c8acc5d89ba3b9d51444c0dfd195a21cf30bd6
lukeschunk <lukeschunk@gmail.com>
2015-12-05 17:04:24 -0700

    changing schema

--- core/server/features/classroom/classroom.server.model.js
+++ core/server/features/classroom/classroom.server.model.js
@@ -2,16 +2,32 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 
-var ClassroomSchema = new Schema({
+    var ClassroomSchema = new Schema({
 
-    className: {
-      type: String
-    },
-    usersInClass: [
-      {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false}
-    ]
+        className: {
+          type: String
+        },
+        usersInClass: [
+          {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false}
+        ],
+        groupsInClass:
+        [
+          {
+            groupName: { type: String },
+            usersInGroup: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false},
+            messagesInGroup:
+            [
+              {
+                content: { type: String},
+                date: {type: Date},
+                sender: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false}
+              }
+            ]
+          }
+        ]
 
 
-});
+    });
+
 
 module.exports = mongoose.model('Classroom', ClassroomSchema);
--- core/server/features/messages/message.server.controller.js
+++ core/server/features/messages/message.server.controller.js
@@ -1,4 +1,7 @@
 var Message = require('./message.server.model');
+var Classroom = require('../classroom/classroom.server.model');
+
+console.log("this is Classroom", Classroom);
 
 exports.getMessages = function (req, res, next) {
 
@@ -12,6 +15,8 @@ exports.getMessages = function (req, res, next) {
 };
 
 exports.postMessage = function (req, res, next) {
+  console.log("this is Classroom", Classroom);
+    console.log("this is req", req);
     console.log("controller is being hit");
     var message = new Message(req.body);
     message.save(function (err) {
